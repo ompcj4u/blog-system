dc15bce09454b39ff6aa7879be34cf626d0eed9c
m.jannesari <m.jannesari@ertebat.io>
2026-07-07 23:30:42 +0330

    Content and Password Tests added

---
+++ src/BlogSystem.UnitTests/Domain/ValueObjects/ContentTests.cs
@@ -0,0 +1,57 @@
+﻿using Domain.ValueObjects;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.UnitTests.Domain.ValueObjects;
+public class ContentTests
+{
+    // happy path
+    [Fact]
+    public void Content_Should_Create_For_Valid_Input()
+    {
+        var text = "This is a valid text for content";
+        var content = new Content(text);
+        content.Value.Should().Be(text);
+    }
+    [Fact]
+    public void Content_Should_Have_Correct_WordCount()
+    {
+        var content = new Content("this is 5 words totally");
+        content.WordCount.Should().Be(5);
+    }
+    [Fact]
+    public void Content_Should_Have_Correct_CharacterCount()
+    {
+        var content = new Content("This is 26 character count");
+        content.CharacterCount.Should().Be(26);
+    }
+    [Fact]
+    public void Content_Should_Truncate_Long_Text_For_Excerpt()
+    {
+        var text = new string('a', 300);
+        var content = new Content(text);
+        content.Excerpt.Should().HaveLength(203); // 200 + "..."
+        content.Excerpt.Should().EndWith("...");
+    }
+    [Fact]
+    public void Content_Should_Be_Same_As_Excerpt_For_Short_Text()
+    {
+        var content = new Content("Short Content Equals to Excerpt");
+        content.Excerpt.Should().Be("Short Content Equals to Excerpt");
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Content_Should_Throw_For_Invalid_Input(string input)
+    {
+        var action = () => new Content(input);
+        action.Should().Throw<ArgumentException>();
+    }
+
+}
---
+++ src/BlogSystem.UnitTests/Domain/ValueObjects/PasswordTests.cs
@@ -0,0 +1,73 @@
+﻿using Domain.ValueObjects;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.UnitTests.Domain.ValueObjects;
+public class PasswordTests
+{
+    [Fact]
+    public void Password_Should_Create_Both_Hash_And_Salt()
+    {
+        var passKey = "MyPassword123";
+        var password = Password.Create(passKey);
+        password.Salt.Should().NotBeNullOrEmpty();
+        password.Hash.Should().NotBeNullOrEmpty();
+        password.Hash.Should().NotBe(passKey);
+    }
+
+    [Fact]
+    public void Password_Verify_Returns_True_For_Correct_Password()
+    {
+        // assign
+        var passKey = "MyPassword123";
+        // act
+        var password = Password.Create(passKey);
+        //assert
+        password.Verify(passKey).Should().BeTrue();
+    }
+    [Fact]
+    public void Password_Generate_Different_Hash_For_Same_Key()
+    {
+        var passKey = "SamePassword";
+        
+        var pass1 = Password.Create(passKey);
+        var pass2 = Password.Create(passKey);
+        pass1.Hash.Should().NotBe(pass2.Hash);
+        pass1.Salt.Should().NotBe(pass2.Salt);
+    }
+
+    [Fact]
+    public void Password_Verify_Returns_False_For_InCorrect_Password()
+    {
+        // assign
+        var passKey = "MyPassword123";
+        var wrongPassKey = "MyPassword456";
+        // act
+        var password = Password.Create(passKey);
+        //assert
+        password.Verify(wrongPassKey).Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData(null)]
+    public void Password_Throw_For_Invalid_Input(string input)
+    {
+        var action = () => Password.Create(input);
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void Password_Throw_For_Short_Input()
+    {
+        var action = () => Password.Create("12345");
+        action.Should().Throw<ArgumentException>();
+        
+    }
+
+}
